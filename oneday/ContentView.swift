6c54f9c5452c7735aed46fddefc535cd25993992
TomasHeideGames <tomasheidel@gmail.com>
2020-06-02 11:57:26 +0200

    initial xcode project creation

---
+++ oneday/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  oneday
+//
+//  Created by Tomas Heide on 2020-06-02.
+//  Copyright © 2020 Tomas Heide. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
